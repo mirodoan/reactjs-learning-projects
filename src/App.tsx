0589e6716cc4a91c9d77beb8c3267ab1c602835a
miro doan <mirodoanit@gmail.com>
2025-08-14 15:51:45 +0700

    add more layout UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 // import { UserProfile } from "./One/UserProfile";
-import { Collections } from "./Three/Collections";
+// import { Collections } from "./Three/Collections";
+import { ReviewForm } from "./Four/ReviewForm";
 // import { JobList } from "./Two/JobList";
 
 function App() {
   return (
-    <h1 className="flex w-full items-center justify-center bg-[#E6EFFA]">
+    <h1 className="flex min-h-screen w-full items-center justify-center bg-[#E6EFFA]">
       {/* <UserProfile /> */}
       {/* <JobList /> */}
-      <Collections />
+      {/* <Collections /> */}
+
+      <ReviewForm />
     </h1>
   );
 }
---
+++ src/Four/Input.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface InputProps {
+  title: string;
+  placeholder: string;
+  type: "text" | "textarea";
+}
+
+export const Input: React.FC<InputProps> = ({ title, placeholder, type }) => {
+  return (
+    <div>
+      <label htmlFor={title} className="block font-poppi text-sm font-semibold">
+        {title}
+      </label>
+      {type === "text" && (
+        <input
+          id={title}
+          placeholder={placeholder}
+          className="mt-1 w-full rounded-md border-2 p-4 py-3"
+        />
+      )}
+
+      {type === "textarea" && (
+        <textarea
+          id={title}
+          placeholder={placeholder}
+          className="mt-1 w-full rounded-md border-2 p-4 py-3"
+        />
+      )}
+    </div>
+  );
+};
